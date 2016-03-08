f1debdddfcc1bbbb82fdf7bb0d447ed28883821a
Shao Sun <ss2cp@virginia.edu>
2016-03-08 14:45:36 -0500

    Adding question 326
    https://leetcode.com/problems/power-of-three/

---
+++ 326. Power of Three/326. Power of Three.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n==1.0: return True
+        if n%3!=0: return False
+        if n == 3: return True
+        n=n*1.0
+        while n>3: 
+            n=n/3
+            if n == 3: return True
+        return False
