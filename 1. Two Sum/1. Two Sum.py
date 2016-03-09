b48e2ee154d8254f70ca099cdea3731b8eb05e69
Shao Sun <ss2cp@virginia.edu>
2016-03-08 19:41:36 -0500

    Adding question 1
    https://leetcode.com/problems/two-sum/

---
+++ 1. Two Sum/1. Two Sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ret=[]
+        for i in range(len(nums)) :
+            if target-nums[i] in nums and i!=nums.index(target-nums[i]): 
+                ret.append(nums.index(target-nums[i]))
+                ret.append(i)
+                break
+        return ret
